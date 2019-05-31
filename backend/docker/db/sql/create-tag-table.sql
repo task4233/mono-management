780b1b3b975b7aaa94d2b083ccb84e9894c464be
task4233 <tks.m1205@gmail.com>
2019-05-31 14:38:23 +0900

    [UPDATE] initlaize file of tag table #2

--- backend/docker/db/sql/create-tag-table.sql
+++ backend/docker/db/sql/create-tag-table.sql
@@ -6,9 +6,14 @@ create table if not exists `Tag`
   `id`             INT(8) AUTO_INCREMENT primary key,
   `name`           VARCHAR(64) NOT NULL,
   `parentId`       INT(8),
+  `userId`         INT(8),
   constraint fk_parentId
     foreign key (`parentId`)
     references `Tag` (`id`)
+    on delete restrict on update restrict,
+  constraint fk_userId
+    foreign key (`userId`)
+    references `User` (`id`)
     on delete restrict on update restrict
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
