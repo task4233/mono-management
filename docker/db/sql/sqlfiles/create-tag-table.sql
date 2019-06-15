7e9c1fc89493c47064157b9721eecafe9899dd46
task4233 <tks.m1205@gmail.com>
2019-06-15 11:36:51 +0900

    [UPDATE] DB names

--- docker/db/sql/sqlfiles/create-tag-table.sql
+++ docker/db/sql/sqlfiles/create-tag-table.sql
@@ -1,24 +1,24 @@
--- Tag Table
+-- tags Table
 /*
-  Depending on User Table & Tag Table
-  Necessary to exist User Table & Tag Table
+  Depending on users Table & tags Table
+  Necessary to exist users Table & tags Table
 */
 
-drop table if exists `Tag`;
+drop table if exists `tags`;
 
-create table if not exists `Tag`
+create table if not exists `tags`
 (
   `id`             INT(8) AUTO_INCREMENT primary key,
   `name`           VARCHAR(64) NOT NULL,
   `parentId`       INT(8),
   `userId`         INT(8) NOT NULL,
-  constraint foreignKey_parentId_from_Tag_to_Tag
+  constraint foreignKey_parentId_from_tags_to_tags
     foreign key (`parentId`)
-    references `Tag` (`id`)
+    references `tags` (`id`)
     on delete restrict on update restrict,
-  constraint foreignKey_userId_from_Tag_to_User
+  constraint foreignKey_userId_from_tags_to_users
     foreign key (`userId`)
-    references `User` (`id`)
+    references `users` (`id`)
     on delete restrict on update restrict
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
