6a7c5db036811f1a7394958cb9329d4440bcb852
ka-tsu-mo <46083154+ka-tsu-mo@users.noreply.github.com>
2019-06-16 12:20:42 +0900

    Add go standard (#8)
    * [ADD] some files
    
    * Split main.go into parts
    
    * [UDPATE] divided files
    
    * [UPDATE] .gitignore
    
    * [UPDATE] .gitignore

---
+++ backend/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	// routes
+	// relative path from ./vender
+	//"github.com/task4233/mono-management/backend/router"
+
+    // router
+	"github.com/task4233/mono-management/backend/router"
+)
+
+func main() {
+
+	router := router.Create()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// shutdown ther server with a timeout of 5 seconds
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ... ")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
+
+}
