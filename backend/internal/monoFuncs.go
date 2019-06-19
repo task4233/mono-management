25a3c8b387ff6c1e596682be34065867ab6e3eb4
task4233 <tks.m1205@gmail.com>
2019-06-20 08:32:25 +0900

    [ADD] comments for monoFuncs.go

--- backend/internal/monoFuncs.go
+++ backend/internal/monoFuncs.go
@@ -1,55 +1,75 @@
 package internal
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// User comment...
-type (
-	User struct {
-		Name string `db:"name" json:"name"`
-		Pass string `db:"pass" json:"pass"`
-	}
-)
+/*
+GetMonos は, itemsテーブルからMonoの情報を集めて返却するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
 
-// GetMonos get
+Endpoints
+
+	[GET]	/api/v1/mono
+*/
 func GetMonos(c *gin.Context) {
-	// samp
+	// Monoデータをitemsテーブルから取得する
+
+	// itemsテーブルの全てのデータを持ってくる
+
+	// itemsテーブルのidごとに, itemdatasテーブルで該当するデータをくっつける
+
 	c.JSON(http.StatusOK, "get monos")
 }
 
-// CreateMono create
+/*
+CreateMono は, リクエストに含まれる情報をitemsテーブルに追加するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+Endpoints
+
+	[POST]   /api/v1/mono/new
+*/
 func CreateMono(c *gin.Context) {
-	// samp
-	var user User
-	c.Bind(&user)
+	// リクエストに含まれる情報のうち, name, userId, tagIdをitemsテーブルに追加
 
-	log.Println(user)
-
+	// リクエストに含まれる情報のうち, datasに含まれるデータ群を1つずつに対して以下の処理を行う
+	// + type(primary data), name(データ名)をdatasテーブルに追加する
+	// + そのdataId, itemId, 型に該当する値をitemdatasテーブルに追加する
 	c.JSON(http.StatusOK, "create mono")
 }
 
-// UpdateMonos update
+/*
+UpdateMonos は, リクエストに該当するitemsテーブルの情報をまとめて更新するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+Endpoints
+
+	[PUT]    /api/v1/mono/:monoId
+*/
 func UpdateMonos(c *gin.Context) {
-	// samp
-	var user User
-	c.Bind(&user)
+	// パスの{:monoId}を用いてデータをitemsテーブルから持ってくる
 
-	log.Println(user)
-
+	// itemdatasテーブルから{:monoId}を含むデータを全て引き出す
+	// リクエストに含まれる情報のうち,
+	// keyが, dataIdから取得したdatasテーブルのnameと一致するように
+	// itemdatasテーブルのデータを更新する
 	c.JSON(http.StatusOK, "update mono")
 }
 
-// DeleteMono delete
-func DeleteMono(c *gin.Context) {
-	// samp
-	var user User
-	c.Bind(&user)
+/*
+DeleteMono は, リクエストに含まれるuserIdを持つデータをitemsテーブルから削除するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
 
-	log.Println(user)
+Endpoints
+
+	[DELETE] /api/v1/mono/:monoId
+*/
+func DeleteMono(c *gin.Context) {
+	// パスの{:monoId}を用いてデータをitemsテーブルから削除する
+	// 関連するデータ群はまとめて削除される
 
 	c.JSON(http.StatusOK, "delete mono")
 }
