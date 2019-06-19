219d55ffc6968fee57b87e8e1bd6241bb7e2fa64
task4233 <tks.m1205@gmail.com>
2019-06-19 18:40:41 +0900

    [FIXED] double -> float64 & time -> time.Time

--- backend/internal/tools.go
+++ backend/internal/tools.go
@@ -1,9 +1,11 @@
 package internal
 
 import (
-	"os"
 	"net/http"
 	"net/url"
+	"os"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -20,62 +22,61 @@ type (
 		HashedPass string `json:hashedPass`
 	}
 
-    // items
-    Item struct {
-        // id
-        Id int `json:id`
-        // name
-        Name string `json:name`
-        // userId
-        UserId int `json:userId`
-        // tagId
-        TagId int`json:tagId`
-    }
+	// items
+	Item struct {
+		// id
+		Id int `json:id`
+		// name
+		Name string `json:name`
+		// userId
+		UserId int `json:userId`
+		// tagId
+		TagId int `json:tagId`
+	}
 
-    // itemdatas
-    ItemData struct {
-        // dataId
-        DataId int `json:dataId`
-        // itemId
-        ItemId int `json:itemId`
-        // num
-        Num double `json:num`
-        // str
-        Str string `json:str`
-        // timestamp
-        Timestamp time `json:timestamp`
-    }
+	// itemdatas
+	ItemData struct {
+		// dataId
+		DataId int `json:dataId`
+		// itemId
+		ItemId int `json:itemId`
+		// num
+		Num float64 `json:num`
+		// str
+		Str string `json:str`
+		// timestamp
+		Timestamp time.Time `json:timestamp`
+	}
 
-    // tags
-    Tag struct {
-        // id
-        Id int `json:id`
-        // name
-        Name string `json:name`
-        // parentId
-        ParentId int `json:parentId`
-        // userId
-        UserId int `json:userId`
-    }
+	// tags
+	Tag struct {
+		// id
+		Id int `json:id`
+		// name
+		Name string `json:name`
+		// parentId
+		ParentId int `json:parentId`
+		// userId
+		UserId int `json:userId`
+	}
 
-    // datas
-    Data struct {
-        // id
-        Id int `json:id`
-        // name
-        Name string `json:name`
-        // type
-        Type string `json:type`
-    }
+	// datas
+	Data struct {
+		// id
+		Id int `json:id`
+		// name
+		Name string `json:name`
+		// type
+		Type string `json:type`
+	}
 
-    // tokens
-    Token struct {
-        // token
-        Token string `json:token`
-        // userId
-        UserId int `json:userId`
-    }
-    
+	// tokens
+	Token struct {
+		// token
+		Token string `json:token`
+		// userId
+		UserId int `json:userId`
+	}
 )
 
 // HTTP funcs
@@ -89,14 +90,15 @@ func SendDefaultHeader(c *gin.Context, methods string) {
 // Cookie funcs
 func SetCookie(c *gin.Context, name string, val string) {
 	http.SetCookie(c.Writer, &http.Cookie{
-		Name: name,
-		Value: url.QueryEscape(val),
-		MaxAge: 1892160000,
-		Path: "/",
-		Secure: false,
+		Name:     name,
+		Value:    url.QueryEscape(val),
+		MaxAge:   1892160000,
+		Path:     "/",
+		Secure:   false,
 		HttpOnly: true,
 	})
 }
+
 func GetCookie(c *gin.Context, name string) (string, error) {
 	cookie, err := c.Request.Cookie(name)
 	if err != nil {
@@ -106,19 +108,19 @@ func GetCookie(c *gin.Context, name string) (string, error) {
 	return val, nil
 }
 
-
 // DB funcs
 var (
-	db *gorm.DB
+	db  *gorm.DB
 	err error
 )
+
 func InitDB() {
 	DBMS := "mysql"
 	USER := os.Getenv("MYSQL_USER")
 	PASS := os.Getenv("MYSQL_PASSWORD")
 	PROTOCOL := "tcp(db:3306)"
 	DBNAME := os.Getenv("MYSQL_DATABASE")
-	CONNECT := USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME+"?charset=utf8mb4&parseTime=True&loc=Local"
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err = gorm.Open(DBMS, CONNECT)
 	if err != nil {
