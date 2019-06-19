9ffde88250955dd34bff1f61d0c25cbddddcd805
task4233 <tks.m1205@gmail.com>
2019-06-19 13:45:29 +0900

    [UPDATE] column name,  in user table

--- docker/db/sql/sqlfiles/create-user-table.sql
+++ docker/db/sql/sqlfiles/create-user-table.sql
@@ -5,6 +5,6 @@ create table if not exists `users`
 (
   `id`             INT(8) AUTO_INCREMENT,
   `name`           VARCHAR(64) NOT NULL,
-  `hashed_pass`    VARCHAR(64) NOT NULL,
+  `hashedPass`    VARCHAR(64) NOT NULL,
   primary key(`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
