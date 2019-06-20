4bf63e71f7faea03666bd6250670944632b52280
task4233 <tks.m1205@gmail.com>
2019-06-20 22:07:06 +0900

    Add handler comments (#19)
    * [ADD] comment for package router
    
    * [ADD] comments for tools.go,which belongs to package internal
    
    * [ADD] comments for monoFuncs.go
    
    * [ADD] comments for userFuncs.go
    
    * [ADD] comments for tagFunc.go

--- backend/internal/tagFunc.go
+++ backend/internal/tagFunc.go
@@ -6,25 +6,60 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// GetTags get tag
+/*
+GetTags はuserIdが一致するTagを全て取得するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	[GET]    /api/v1/tag/
+*/
 func GetTags(c *gin.Context) {
-	// samp
+	//
 	c.JSON(http.StatusOK, "get tags")
 }
 
-// CreateTag create tag
+/*
+CreateTag はuserIdを付加したTagを全て取得するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	[POST]   /api/v1/tag/new
+*/
 func CreateTag(c *gin.Context) {
-	// samp
 	c.JSON(http.StatusOK, "crate tag")
 }
 
-// UpdateTag update tag
+/*
+UpdateTag はuserIdを付加したTagを更新するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	[PUT]    /api/v1/tag/:tagId
+*/
 func UpdateTag(c *gin.Context) {
 	// samp
 	c.JSON(http.StatusOK, "update tag")
 }
 
-// DeleteTag delete tag
+/*
+DeleteTag tagIdを元にデータを削除するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	[DELETE] /api/v1/tag/:tagId
+*/
 func DeleteTag(c *gin.Context) {
 	// samp
 	c.JSON(http.StatusOK, "delete tag")
--- backend/internal/userFuncs.go
+++ backend/internal/userFuncs.go
@@ -6,38 +6,103 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// GetInfo get information about user account
+/*
+GetInfo は, ログイン時に保存されるCookieのtoken情報を元にusersテーブルから該当の情報を返却するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	[GET]	/api/v1/user/info
+*/
 func GetInfo(c *gin.Context) {
-	// samp
+	// Cookieのtoken情報を取得
+	// token情報を元にtokensテーブルからuserIdを取得
+	// 該当するuserIdを持つデータをusersテーブルから返却
 	c.JSON(http.StatusOK, "get info")
 }
 
-// Login user login
+/*
+Login は, userIdとパスワードの正当性が確かめられた時にCookieにtokenをセットして返すメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	POST   /api/v1/user/login
+*/
 func Login(c *gin.Context) {
-	// samp
+	// userIdとrawPasswordを取得
+	// rawPasswordをhash関数にかけて, usersテーブルからuserIdを元にhashedPassと照合
+
+	// tokenを生成してtokensテーブルにINSERT
+	// Cookieにtokenをセットして返す
 	c.JSON(http.StatusOK, "login")
 }
 
-// Logout user logout
+/*
+Logout はユーザが保持しているCookieを削除するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	[DELETE]   /api/v1/user/logout
+*/
 func Logout(c *gin.Context) {
-	// samp
+	// tokensテーブルからtokenを削除
+	// 削除できたらおk
 	c.JSON(http.StatusOK, "logout")
 }
 
-// CreateAccount create user account
+/*
+CreateAccount はリクエストのデータでユーザを新たに作成するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	[POST]   /api/v1/user/new
+*/
 func CreateAccount(c *gin.Context) {
-	//samp
+	// userIdとrawPasswordを取得
+
+	// rawPasswordをハッシュ関数にかけてhashedPassを取得
+	// tokensテーブルにhashedPass, userIdを格納
 	c.JSON(http.StatusOK, "create account")
 }
 
-// UpdateAccount update user information
+/*
+UpdateAccount はリクエストのデータでユーザ情報を更新するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	[PUT]   /api/v1/user/
+*/
 func UpdateAccount(c *gin.Context) {
-	// samp
+	// usersテーブルからuserIdを元に該当データをupdate
+
 	c.JSON(http.StatusOK, "update account")
 }
 
-// DeleteAccount delete user account
+/*
+DeleteAccount は該当アカウントを削除するメソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	[DELETE]   /api/v1/user/
+*/
 func DeleteAccount(c *gin.Context) {
-	// samp
+	// usersテーブルからuserIdを元に該当データをdelete
 	c.JSON(http.StatusOK, "delete account")
 }
