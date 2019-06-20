5db52e29a92f900427178f1092e45714ab97b0b0
task4233 <tks.m1205@gmail.com>
2019-06-20 22:07:06 +0900

    Add handler comments (#19)
    * [ADD] comment for package router
    
    * [ADD] comments for tools.go,which belongs to package internal
    
    * [ADD] comments for monoFuncs.go
    
    * [ADD] comments for userFuncs.go
    
    * [ADD] comments for tagFunc.go

--- backend/internal/searchFunc.go
+++ backend/internal/searchFunc.go
@@ -6,8 +6,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// SearchMonos search mono data
+/*
+SearchMonos は, メソッドです.
+途中でエラーが発生した場合の挙動はwikiを参照してください.
+
+
+Endpoints
+
+
+	[POST]   /api/v1/search/
+*/
 func SearchMonos(c *gin.Context) {
-	//samp
+	// リクエストに含まれる検索文字列をitemsテーブルから取得
+	// データを返却
 	c.JSON(http.StatusOK, "search")
 }
--- backend/internal/tools.go
+++ backend/internal/tools.go
@@ -1,3 +1,10 @@
+/*
+Package internal は, Ginフレームワークのハンドラの設定等をするパッケージです.
+
+docker-composeの環境下では
+	import app/internal
+でimportできます.
+*/
 package internal
 
 import (
@@ -11,6 +18,12 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+// DB funcs
+var (
+	db  *gorm.DB
+	err error
+)
+
 type (
 	// users
 	User struct {
@@ -18,7 +31,7 @@ type (
 		Id int `json:id`
 		// name
 		Name string `json:name`
-		// hashed_pass
+		// hashedPass
 		HashedPass string `json:hashedPass`
 	}
 
@@ -79,41 +92,11 @@ type (
 	}
 )
 
-// HTTP funcs
-func SendDefaultHeader(c *gin.Context, methods string) {
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Methods", methods)
-	c.Header("Access-Control-Max-Age", "86400")
-	c.Header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-}
+/*
+InitDB は, 内部でGorm.Open()を行うメソッドです.
 
-// Cookie funcs
-func SetCookie(c *gin.Context, name string, val string) {
-	http.SetCookie(c.Writer, &http.Cookie{
-		Name:     name,
-		Value:    url.QueryEscape(val),
-		MaxAge:   1892160000,
-		Path:     "/",
-		Secure:   false,
-		HttpOnly: true,
-	})
-}
-
-func GetCookie(c *gin.Context, name string) (string, error) {
-	cookie, err := c.Request.Cookie(name)
-	if err != nil {
-		return "", err
-	}
-	val, _ := url.QueryUnescape(cookie.Value)
-	return val, nil
-}
-
-// DB funcs
-var (
-	db  *gorm.DB
-	err error
-)
-
+このメソッドはmain関数で一度呼び, Gormの機能を使用したい場合はGetDB()でdbを受け取ってから処理することを推奨しています.
+*/
 func InitDB() {
 	DBMS := "mysql"
 	USER := os.Getenv("MYSQL_USER")
@@ -127,6 +110,86 @@ func InitDB() {
 		panic(err.Error())
 	}
 }
+
+/*
+GetDB は, InitDB()でGorm.Open()したdbのデータのみを受け取るメソッドです.
+
+このメソッドを使用する際はInitDB()を必ず呼び出してください.
+
+以下がサンプルです.
+	db := GetDB()
+
+	var users []User
+	if err := db.Find(&users).Error; err != nil {
+		log.Fatal(err.Error)
+	}
+*/
 func GetDB() *gorm.DB {
 	return db
+}
+
+/*
+SendDefaultHeader は, JSON Headerに情報を付加するメソッドです.
+
+第2引数に許可するHTTPメソッドを渡します.
+
+以下がサンプルです.
+	// GETメソッドのみを許可
+	SendDefaultHeader(c, "GET")
+	// GETおよびPOSTメソッドを許可
+	SendDefaultHeader(c, "GET/POST")
+*/
+func SendDefaultHeader(c *gin.Context, methods string) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", methods)
+	c.Header("Access-Control-Max-Age", "86400")
+	c.Header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+/*
+SetCookie は, Cookie情報を付加するメソッドです.
+
+第2引数にCookie名を, 第三引数にCookieに付加する値を渡します.
+
+以下がサンプルです.
+	// HogeというCookie名で, Fugaという値を保持
+	SetCookie(c, "Hoge", "Fuga")
+
+*/
+func SetCookie(c *gin.Context, name string, val string) {
+	http.SetCookie(c.Writer, &http.Cookie{
+		Name:     name,
+		Value:    url.QueryEscape(val),
+		MaxAge:   1892160000,
+		Path:     "/",
+		Secure:   false,
+		HttpOnly: true,
+	})
+}
+
+/*
+GetCookie は, Cookie情報を読み取るメソッドです.
+
+第2引数にCookie名を渡します.
+
+第1返り値に読み取ったCookie情報を, 第2返り値にエラーを返します.
+
+以下がサンプルです.
+	// HogeというCookie名の情報を読み取る.
+	// 返り値としてcookie_valueというCookie情報と, errというエラーを受け取る.
+	cookie_value, err := GetCookie(c, "Hoge")
+
+	// エラーハンドリング
+	if err != nil {
+		log.Fatal(err.Error)
+	}
+
+*/
+func GetCookie(c *gin.Context, name string) (string, error) {
+	cookie, err := c.Request.Cookie(name)
+	if err != nil {
+		return "", err
+	}
+	val, _ := url.QueryUnescape(cookie.Value)
+	return val, nil
 }
