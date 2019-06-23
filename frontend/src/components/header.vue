7e527cebf7f875a64295c147bd0dfd2c0bf49efa
MissionaryPS <red731_NinaP@outlook.jp>
2019-06-24 03:24:12 +0900

    template記述

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -1,11 +1,16 @@
 <template lang="html">
-
+  <search></search>
+  {{headLabel}}
+  <menu></menu>
 </template>
 
 <script>
 import search from '@component/search.vue'
 import menu from '@component/menu.vue'
 export default {
+  props:{
+    headLabel:'hoge'
+  }
 }
 </script>
 
