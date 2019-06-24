2c660a1ad82a82b3df5d00201fbe1bdfe4257ebe
shirakawa_main <shota5038@icloud.com>
2019-06-24 15:53:17 +0900

    router.jsをsrc直下にした

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
+import router from './router.js'
 
 Vue.config.productionTip = false
 /* eslint-disable no-new */
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
-import Login from '../components/login.vue'
-import SignUp from '../components/signup.vue'
+import Login from './components/login.vue'
+import SignUp from './components/signup.vue'
 
 Vue.use(Router)
 
