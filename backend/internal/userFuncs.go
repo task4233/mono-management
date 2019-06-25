3f63b7098b91ab2590bdd5a8ee70b5e81eb731a2
ka-tsu-mo <funfun.tsukasa@gmail.com>
2019-06-25 13:37:45 +0900

    add helper functions

--- backend/internal/userFuncs.go
+++ backend/internal/userFuncs.go
@@ -11,6 +11,11 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+type account struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
 /*
 GetInfo は, ログイン時に保存されるCookieのtoken情報を元にusersテーブルから該当の情報を返却するメソッドです.
 途中でエラーが発生した場合の挙動はwikiを参照してください.
@@ -26,16 +31,25 @@ func GetInfo(c *gin.Context) {
 	// Cookieのtoken情報を取得
 	// token情報を元にtokensテーブルからuserIdを取得
 	// 該当するuserIdを持つデータをusersテーブルから返却
+	SendDefaultHeader(c, "GET")
 	db := GetDB()
-	token, err := GetCookie(c, "token")
+
+	user, err := CheckLogin(c)
 	if err != nil {
+		return
+	}
+
+	if err := db.Where("userId = ?", user.Id).First(&user).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"status":  false,
-			"message": "サーバエラー",
+			"message": "ユーザが見つかりません",
 		})
 		return
 	}
-	c.JSON(http.StatusOK, "get info")
+	c.JSON(http.StatusOK, gin.H{
+		"status": true,
+		"user":   user,
+	})
 }
 
 /*
@@ -48,18 +62,25 @@ Endpoints
 
 	POST   /api/v1/user/login
 */
-func Login(c *gin.Context, name string, password string) {
+func Login(c *gin.Context) {
 	// userIdとrawPasswordを取得
 	// rawPasswordをhash関数にかけて, usersテーブルからuserIdを元にhashedPassと照合
 
 	// tokenを生成してtokensテーブルにINSERT
 	// Cookieにtokenをセットして返す
+	SendDefaultHeader(c, "POST")
+	var json account
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  false,
+			"message": err.Error(),
+		})
+		return
+	}
 	db := GetDB()
 	user := User{}
 
-	SendDefaultHeader(c, "POST")
-
-	if err := db.Where("name = ?", name).First(&user).Error; err != nil {
+	if err := db.Where("name = ?", json.Name).First(&user).Error; err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"status":  false,
 			"message": "入力したユーザ名が存在しません",
@@ -67,7 +88,7 @@ func Login(c *gin.Context, name string, password string) {
 		return
 	}
 
-	err := bcrypt.CompareHashAndPassword([]byte(user.HashedPass), []byte(password))
+	err := bcrypt.CompareHashAndPassword([]byte(user.HashedPass), []byte(json.Password))
 	if err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"status":  false,
@@ -105,14 +126,12 @@ Endpoints
 func Logout(c *gin.Context) {
 	// tokensテーブルからtokenを削除
 	// 削除できたらおk
+	SendDefaultHeader(c, "DELETE")
 	db := GetDB()
 	user, err := CheckLogin(c)
-
 	if err != nil {
 		return
 	}
-
-	SendDefaultHeader(c, "DELETE")
 
 	token := Token{}
 	if err := db.Where("userId = ?", user.Id).First(&token).Error; err != nil {
@@ -139,12 +158,20 @@ Endpoints
 
 	[POST]   /api/v1/user/new
 */
-func CreateAccount(c *gin.Context, name string, password string) {
+func CreateAccount(c *gin.Context) {
 	// userIdとrawPasswordを取得
 
 	// rawPasswordをハッシュ関数にかけてhashedPassを取得
 	// tokensテーブルにhashedPass, userIdを格納
-	hasedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	SendDefaultHeader(c, "POST")
+	var json account
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  false,
+			"message": err.Error(), // 仮
+		})
+	}
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(json.Password), bcrypt.DefaultCost)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"status":  false,
@@ -152,6 +179,10 @@ func CreateAccount(c *gin.Context, name string, password string) {
 		})
 		return
 	}
+
+	user := User{}
+	user.Name = json.Name
+	user.HashedPass = string(hashedPass)
 
 	c.JSON(http.StatusOK, "create account")
 }
@@ -192,34 +223,34 @@ GetUserFromToken はトークンからユーザ情報を取得するメソッド
 途中でエラーが発生した場合の挙動はwikiを参照してください．
 
 */
-func GetUserFromToken(token string) (*User, error) {
+func GetUserFromToken(token string) (User, error) {
 	if len(token) == 0 {
-		return &User{Id: -1, Name: "", HashedPass: ""}, errors.New("empty token")
+		return User{Id: -1}, errors.New("empty token")
 	}
 	var userToken Token
 	if err := GetDB().Where(&Token{Token: token, UserId: 0}).First(&userToken).Error; err != nil {
 		if gorm.IsRecordNotFoundError(err) {
-			return &User{Id: -1, Name: "", HashedPass: ""}, errors.New("login required")
+			return User{Id: -1}, errors.New("login required")
 		}
-		return &User{Id: -1, Name: "", HashedPass: ""}, errors.New("something went wrong")
+		return User{Id: -1}, errors.New("something went wrong")
 	}
 	var user User
 	if err := GetDB().Where(&User{Id: userToken.UserId, Name: "", HashedPass: ""}).First(&user).Error; err != nil {
-		return &User{Id: -1, Name: "", HashedPass: ""}, errors.New("something went wrong")
+		return User{Id: -1}, errors.New("something went wrong")
 	}
-	return &user, nil
+	return user, nil
 }
 
 /*
 GetUserFromCookie はCookieからユーザ情報を取得するメソッドです．
 途中でエラーが発生した場合の挙動はwikiを参照してください．
 */
-func GetUserFromCookie(c *gin.Context) (*User, error) {
+func GetUserFromCookie(c *gin.Context) (User, error) {
 	token, err := GetCookie(c, "token")
 	if err != nil {
 		return GetUserFromToken(token)
 	} else {
-		return &User{Id: -1, Name: "", HashedPass: ""}, errors.New("Login Required")
+		return User{Id: -1}, errors.New("Login Required")
 	}
 }
 
@@ -227,7 +258,7 @@ func GetUserFromCookie(c *gin.Context) (*User, error) {
 CheckLogin はログイン状態の確認をするメソッドです．
 途中でエラーが発生した場合の挙動はwikiを参照してください．
 */
-func CheckLogin(c *gin.Context) (*User, error) {
+func CheckLogin(c *gin.Context) (User, error) {
 	user, err := GetUserFromCookie(c)
 	if err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{
