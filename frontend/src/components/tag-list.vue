3491b4ded4ab03d625fd4a56b4bd5f8305a512d0
MissionaryPS <red731_NinaP@outlook.jp>
2019-07-01 14:40:17 +0900

    直前のcommitに入れ忘れた．プルダウンリストの実装

---
+++ frontend/src/components/tag-list.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <select v-model="selected">
+    <option v-for "option in headLabels" v-bind:value="option.id">
+      {{option.name}}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  data:{
+    selected:null
+  },
+  computed:{
+    headLabel(){
+      this.$store.tag_list
+    }
+  },
+  created:{
+    this.$store.dispatch('getTagList')
+    this.$store.dispatch('getMonoList', null, null)
+  },
+  watch:{
+    selected(){
+      this.$store.dispach('getMonoList', selected, null)
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
