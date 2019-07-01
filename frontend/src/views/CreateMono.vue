843fc738fef28b9752c7a5fbffc74d7f621b3414
task4233 <tks.m1205@gmail.com>
2019-07-02 02:00:21 +0900

    [UPDATE] stasssssssssssshhhhhhhhhhhhhhhhh

--- frontend/src/views/CreateMono.vue
+++ frontend/src/views/CreateMono.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="signup">
     <h1>新規作成</h1>
-    <input type="text" placeholder="名前" v-model="name" />
-    <input type="text" placeholder="タグ" v-model="tagId" />
+    <p>
+      <input type="text" placeholder="名前" v-model="name" />
+      <input type="text" placeholder="タグ" v-model="tagId" />
+    </p>
+
+    <div class="dynamic">
+      <p v-for="data in datas" :key="name">
+        <input type="text" v-model="data.name" />
+        <input type="text" v-model="data.type" />
+        <input type="text" v-model="data.value" />
+      </p>
+      <p>
+        <input type="text" placeholder="新しい要素名を追加！" v-model="dataName" />
+        <input type="text" placeholder="新しい要素の型を選択" v-model="dataType" />
+        <input type="text" placeholder="新しい要素の値を追加！" v-model="dataValue" />
+      </p>
+    </div>
+    <button @click="addData" class="btn btn-primary btn-sm">+</button>
     <button @click="create">保存する</button>
   </div>
 </template>
@@ -13,14 +29,42 @@ import qs from "qs";
 
 export default {
   name: "CreateMono",
+  data: {
+    dataName: "",
+    dataValue: "",
+    dataType: "",
+    data: []
+  },
   methods: {
+    addData: function() {
+      const newDataName = this.dataName.trim();
+      if (!newDataName) {
+        return;
+      }
+      const newDataType = this.dataType.trim();
+      if (!newDataType) {
+        return;
+      }
+      const newDataValue = this.dataValue.trim();
+      if (!newDataValue) {
+        return;
+      }
+      this.data.push({
+        name: newDataName,
+        type: newDataType,
+        value: newDataValue
+      });
+      alert("呼ばれたよ!");
+      this.dataName = " ";
+      this.dataType = " ";
+      this.dataValue = " ";
+    },
     create: function() {
       const data = {
         name: String(this.name),
         tagId: Number(this.tagId)
-      }
-      console.log(data)
-
+      };
+      console.log(data);
 
       Axios.post("/api/v1/mono/new", data, {
         headers: {
