7ba6184fb1df8436e7a9676d91480d95534b62f9
shirakawa36 <49920756+shirakawa36@users.noreply.github.com>
2019-07-02 15:19:16 +0900

    Add vue login kai (#35)
    * backend指定のAPIエンドポイントでログイン。サインアップ処理の追加
    
    * watchで、ID又はPASS空白時、サーバ接続不可時のエラーを出力可能にした。
    
    * マキナス4 squareをフォント指定するためにファイルをfrontend直下に追加
    
    * I want to use Makinas-4-Square font.So I do followings.add files,folders that frontend/src/assets/fonts/Makinas-4-Square.otf and frontend/src/assets/sass/style.scss. add acorn^6.0.0 to package.json.
    
    * 文字数制限の追加、マキナスフォントが実装できない、全角文字の制限ができない。
    
    * サインアップ画面もログイン画面と同等レベルまで完成
    
    * 制限文字数を64に設定。
    
    * 制限文字数を64に設定。
    
    * 404問題を解決
    
    * ログインページをいい感じに
    
    * 改行コードの部分的な統一及びrouter.jsの修正

---
+++ frontend/src/components/signup.vue
@@ -0,0 +1,99 @@
+<template>
+  <b-container class="signup">
+    <h1>mono management</h1>
+    <b-form-group>
+      <b-button variant="outline-success" class="mx-auto" href="/">login</b-button>
+    </b-form-group>
+    <b-form-group>
+      <div class="signupId">
+        <b-form-input type="text" placeholder="ユーザ名" v-model="signupId"></b-form-input>
+      </div>
+    </b-form-group>
+    <b-form-group>
+      <div class="signupPass">
+        <b-form-input type="password" placeholder="パスワード" v-model="signupPass"></b-form-input>
+      </div>
+    </b-form-group>
+    <b-form-group>
+      <div class="signupPassRetype">
+        <b-form-input type="password" placeholder="パスワード再入力" v-model="signupPassRetype"></b-form-input>
+      </div>
+    </b-form-group>
+    <b-form-group>
+      <b-button v-on:click="signup" variant="primary" class="mx-auto">サインアップ</b-button>
+    </b-form-group>
+    <b-row class="errorForm">
+      <p v-if="error" class="mx-auto">{{ error }}</p>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'signup',
+  data: function() {
+    return {
+      signupId: '',
+      signupPass: '',
+      signupPassRetype: '',
+      error : '',
+      flag : 0, //  flag変数をloginされるたびに変えて、watchを呼び出す。
+      server : 0
+    }
+  },
+  watch : {
+    flag: function () {
+      this.error = ''; // errorの初期化
+      if (this.signupId === '') {
+        this.error = this.error + 'ユーザ名が入力されていません。'
+      }
+      if (this.signupId.length > 64) {
+        this.error = this.error + 'ユーザ名が長すぎます。'
+      }
+      if (this.signupPass === '') {
+        this.error = this.error + 'パスワードが入力されていません。'
+      }
+      if (this.signupPass !== this.signupPassRetype) {
+        this.error = this.error + 'パスワード再入力が一致していません。'
+      }
+      if (this.signupPass.length > 64) {
+        this.error = this.error + 'パスワードが長すぎます。'
+      }
+      if (this.server === 1) {
+        this.error = this.error + 'サーバに接続できませんでした。'
+      }
+    }
+  },
+  methods: {
+    signup: function() {
+      var self = this;
+      this.flag++;
+      if (this.signupId !== '' && this.signupPass !== '' && this.signupId.length <= 64 && this.signupPass.length <= 64) {
+        var data = {name : this.signupId, password : this.signupPass };
+        axios.post('/api/v1/user/new', data)
+          .then(response => {
+            console.log('body:', response.data);
+          }).catch(function(error) {
+            console.log(error);
+            self.server = 1;
+            self.flag = 0;
+          });
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.signup {
+  font-family: 'Makinas-4-Square';
+}
+
+.errorform {
+  /*margin: 0 auto;
+  width: 250px;
+  height: 200px;
+  background-color: #f0f0f0;*/
+}
+</style>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from './components/login.vue'
+import SignUp from './components/signup.vue'
+import Header from './components/HelloWorld.vue'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
@@ -10,16 +12,22 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/login',
+      name: 'login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/signup',
+      name: 'signup',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+      component: SignUp
+    },
+    {
+      path: '/header',
+      name: 'header',
+      component: Header
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
