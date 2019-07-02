e145464eaeaabcc1e65aa19eab08c1e63be9b793
task4233 <tks.m1205@gmail.com>
2019-07-02 11:59:17 +0900

    [ADD] list page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVuew)
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 v-if="itemdatas.length === 0">モノが1つも無いようです</h1>
+    <p v-else v-for="dat in itemdatas" :key="dat.name">
+      <router-link v-bind:to="{name: 'mono', params: {id: dat.id}}">{{ dat.name }}</router-link>
+    </p>
+    <!-- <b-button size="lg" variant="outline-primary" @click="rerun">リロード用ボタン</b-button> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      itemdatas: []
+    };
+  },
+  created: function() {
+    Axios.get("/api/v1/mono/")
+      .then(response => {
+        console.log(response);
+        const stat = response.data.Status;
+        if (stat) {
+          const datas = response.data.Data;
+          console.log(datas);
+          console.log(datas.length);
+          for (let i = 0; i < datas.length; ++i) {
+            const mid = datas[i].Id;
+            const mname = datas[i].name;
+            const mtagId = datas[i].tagId;
+            const muserId = datas[i].userId;
+            console.log({
+              id: mid,
+              name: mname,
+              tagId: mtagId,
+              userId: muserId
+            });
+            this.itemdatas.push({
+              id: mid,
+              name: mname,
+              tagId: mtagId,
+              userId: muserId
+            })
+          }
+          this.$router.push("/");
+        }
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
   }
 }
 </script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.signup {
+  margin-top: 20px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+}
+input {
+  margin: 10px 0;
+  padding: 10px;
+}
+</style>
