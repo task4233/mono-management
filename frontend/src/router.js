f30a61da581b59d3520cacc116630cdbc2487c7e
task4233 <tks.m1205@gmail.com>
2019-07-02 13:16:35 +0900

    [ADD] update page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import EditMono from './views/EditMono.vue'
+import ShowMono from './views/ShowMono.vue'
 
 Vue.use(Router)
 
@@ -20,6 +22,16 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/mono/:id',
+      name: 'editmono',
+      component: EditMono
+    },
+    {
+      path: '/mono/:id',
+      name: 'showmono',
+      component: ShowMono
     }
   ]
 })
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1 v-if="itemdatas.length === 0">モノが1つも無いようです</h1>
     <p v-else v-for="dat in itemdatas" :key="dat.name">
-      <router-link v-bind:to="{name: 'mono', params: {id: dat.id}}">{{ dat.name }}</router-link>
+      <router-link :to="'/mono/' + dat.id">{{ dat.name }}</router-link>
     </p>
     <!-- <b-button size="lg" variant="outline-primary" @click="rerun">リロード用ボタン</b-button> -->
   </div>
@@ -21,23 +21,14 @@ export default {
   created: function() {
     Axios.get("/api/v1/mono/")
       .then(response => {
-        console.log(response);
         const stat = response.data.Status;
         if (stat) {
           const datas = response.data.Data;
-          console.log(datas);
-          console.log(datas.length);
           for (let i = 0; i < datas.length; ++i) {
             const mid = datas[i].Id;
             const mname = datas[i].name;
             const mtagId = datas[i].tagId;
             const muserId = datas[i].userId;
-            console.log({
-              id: mid,
-              name: mname,
-              tagId: mtagId,
-              userId: muserId
-            });
             this.itemdatas.push({
               id: mid,
               name: mname,
@@ -45,7 +36,6 @@ export default {
               userId: muserId
             })
           }
-          this.$router.push("/");
         }
       })
       .catch(err => {
