392f2f4bb29ed56afe7bc115563e6dffb0265ea9
MissionaryPS <red731_NinaP@outlook.jp>
2019-07-05 00:30:25 +0900

    リストの先頭に[全てのタグ]を追加，それにより表面化したバグを修正

--- frontend/src/components/tag-list.vue
+++ frontend/src/components/tag-list.vue
@@ -1,5 +1,6 @@
 <template lang="html">
   <select v-model="selected">
+    <option value="0">全てのタグ</option>
     <option v-for="option in headLabels" v-bind:value="option.id" :key="option.id">
       {{ option.name}}
     </option>
@@ -11,7 +12,7 @@ export default {
   data: function() {
     return {
       selected: null
-    };
+    }
   },
   computed: {
     headLabels() {
@@ -23,10 +24,12 @@ export default {
   },
   watch: {
     selected() {
-      this.$store.dispach("getMonoList", this.selected, null);
+      console.log(this.selected)
+      this.$store.dispatch("getMonoList", {name:null, tagId:this.selected})
     }
   }
-};
+}
+
 </script>
 
 <style lang="css" scoped>
