27997c31add00ffd7889123f514648ec83423c39
MissionaryPS <red731_NinaP@outlook.jp>
2019-07-05 19:06:11 +0900

    ガワの作成．要策定：編集ウィンドウの表示方法

---
+++ frontend/src/views/EditTags.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div>
+    <li v-for="tag in tags" :key="tag.id" v-on:click="">
+  </div>
+</template>
+
+<script>
+export default {
+  computed:{
+    tags(){
+      this.$store.state.tag_list
+    }
+  },
+  method:{
+    
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
