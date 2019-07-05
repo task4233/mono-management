661590f97fc7cf8f9132347f0ac3723a3da6ffd4
MissionaryPS <red731_NinaP@outlook.jp>
2019-07-05 19:42:08 +0900

    みす．修正

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/login.vue'
 import SignUp from './components/signup.vue'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
