db07e0dfaa89389319c56868c5363a78033ced41
MissionaryPS <red731_NinaP@outlook.jp>
2019-07-08 00:21:10 +0900

    そもそもメニューにloginリンクいらんやろ(暴言)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/mono/new">Create</router-link> |
-      <router-link to="/login">Login</router-link>
+      <router-link to="/mono/new">Create</router-link>
     </div>
     <router-view/>
   </div>
