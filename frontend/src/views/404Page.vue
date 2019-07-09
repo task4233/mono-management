f0d43cba0eefa2dd90b13a2458a4640d335af234
shirakawa_main <shota5038@icloud.com>
2019-07-09 14:38:30 +0900

    サーバとの接続ができていないときの処理をlogin.vue,signup.vueに追加、簡単なviews/404Page.vueを作成、router.jsにこのページのパスを追加

---
+++ frontend/src/views/404Page.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="404">
+    <h1>404 Not Found</h1>
+    <h2>お探しのページは見つかりませんでした。</h2>
+    <h3>あなたがアクセスしようとしたページは存在しない可能性が高いです。</h3>
+  </div>
+</template>
+
+<style>
+.class {
+  
+}
+</style>
