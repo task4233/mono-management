a6f1aaf91f51f0fbcd31e2260aed6af46d35b8c6
shirakawa36 <49920756+shirakawa36@users.noreply.github.com>
2019-07-13 16:19:23 +0900

    Login error custom (#74)
    * login.vue、アカウントがすでに存在した場合を想定したエラー処理を追加
    
    * 修正

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -67,13 +67,20 @@ export default {
       if (this.loginId !== '' && this.loginPass !== '' && this.loginId.length <= 64 && this.loginPass.length <= 64) {
         var data = {name : this.loginId, password : this.loginPass };
         axios.post('/api/v1/user/login', data)
-          .then(function(response){
-            console.log('body:', response.data); // サーバに送信したデータをコンソールに表示
-            router.push('/')
+          .then(response => {
+            console.log(response)
+            if (response.data.status) {
+              console.log('body:', response.data);
+              router.push('/')
+            } else {
+              self.server = response.message
+              self.flag = -1
+            }
           }).catch(function(error) {
             console.log(error); // 通信エラーをコンソールに表示
             self.server = error.response.status; // Vueの中にaxiosが入れ子になっているため、参照できない => thisを変数selfにする
             if (self.server === 404) self.server = 'サーバとの接続が確立できませんでした。'
+            else self.server = error;
             self.flag  = -1;
             console.log(error.response.data.message)
           });
