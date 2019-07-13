bdadb90a19613089d5ffc0557147a4a1bf77d00d
MissionaryPS <red731_NinaP@outlook.jp>
2019-07-13 15:13:13 +0900

    navbarにmenuの中身を移動

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -1,20 +1,20 @@
 <template lang="html">
   <div>
     <b-navbar toggleable="lg" type="dark" variant="success" class="mb-3">
-      <b-navbar-brand to="/about">mono-management</b-navbar-brand>
+      <b-navbar-brand to="/">mono-management</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse" class="ml-auto"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/">Home</b-nav-item>
-          <b-nav-item to="/about">About</b-nav-item>
-          <b-nav-item to="/mono/new">Create</b-nav-item>
           <b-nav-item to="/EditTags">Tags</b-nav-item>
+          <b-nav-item @click="logout">Logout</b-nav-item>
+          <b-nav-item href="https://hackmd.io/giR3aLTXSH6VUQigiqEK0A" target="_blank">How to use</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-form @submit.prevent="dummy">
             <b-form-input size="sm" class="mr-sm-2" type="text"
-              placeholder="Search" maxlength="64" v-model="keyword" 
+              placeholder="Search" maxlength="64" v-model="keyword"
               @keyup="search" @keyup.esc="exitSearch">
             </b-form-input>
             <b-button size="sm" class="my-2 my-sm-0" type="submit"
@@ -28,7 +28,6 @@
   </div>
 </template>
 <script>
-import router from '../router'
 
 export default {
   data:function(){
@@ -43,17 +42,23 @@ export default {
       // inputでenterを押した時のリロードを抑止する役割
     },
     search(){
-      router.push("/")
       const searchdata = {
         name:this.keyword,
         tagId:0
       }
       this.$store.dispatch('getMonoList', searchdata)
+      this.$router.push("/")
     },
     exitSearch(){
       this.keyword=''
 
-    }
+    },
+    logout:function(){
+      this.$axios.delete('api/v1/user/logout')
+      this.$store.commit('resetUserData')
+      this.$router.push({path:'/login'})
+    },
+
 
   }
 }
