b48df5f8564eb7a3182ee774898248d37bbd523b
MissionaryPS <39906446+MissionaryPS@users.noreply.github.com>
2019-07-14 20:58:53 +0900

    Impl navbar suggest (#79)
    navbarの内容を向上
    内容を
    *Home
    *Tags
    *Logout
    *How to use
    に変更．
    うちLogoutは確認を要求するようにmodal windowを表示するよう実装．
    
    以下，コミットログ
    * Renew navbar and implement search form in navbar
    
    * navbarにmenuの中身を移動
    
    * nav-barをログイン・サインアップ・Notfoundページで表示しない
    
    * conflict解消時に誤った操作をしたので修正
    
    * ログアウト確認モーダルウィンドウ実装

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -1,8 +1,6 @@
 <template lang="html">
   <div class="index">
-    <search></search>
     <tagList></tagList>
-    <menuBar></menuBar>
     <monoList></monoList>
   </div>
 </template>
@@ -10,16 +8,12 @@
 <script>
 import monoList from '../components/mono-list.vue'
 import tagList from '../components/tag-list.vue'
-import search from '../components/search.vue'
-import menuBar from '../components/menu.vue'
 
 export default {
   name:'index',
   components:{
     monoList,
     tagList,
-    search,
-    menuBar
   }
 }
 </script>
