e6a822add983466041ca0aa5f0094ac33d14ffbf
shirakawa_main <shota5038@icloud.com>
2019-07-14 15:51:59 +0900

    login.vueに直接errorModalを書き込んで実装成功。でも、errorModal.vueから呼び出して実行するやり方がわからない・

---
+++ frontend/src/components/errorModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="errMOD">
+    <b-modal ref="err-modal" hide-footer title="Error Message">
+      <div class="d-block text-center">
+        <h3>Error</h3>
+        <p>{{errmes}}</p>
+      </div>
+      <b-button class="mt-3" variant="outline-danger" block @click="hideModal">OK</b-button>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+  export default {
+    name : 'errMOD',
+    data : function(){
+      return{
+        errmes : ''
+      }
+    },
+    methods: {
+      showModal() {
+        this.$refs['err-modal'].show()
+      },
+      hideModal() {
+        this.$refs['err-modal'].hide()
+      }
+    }
+  }
+</script>
