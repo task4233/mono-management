b8e793ddb7f01e551928e06a1e2eee9e0de94575
task4233 <tks.m1205@gmail.com>
2019-07-14 09:44:39 +0900

    [FIXED] ambiguous description in 404 page

--- frontend/src/views/404Page.vue
+++ frontend/src/views/404Page.vue
@@ -2,7 +2,7 @@
   <div class="errorForm">
     <h1>404 Not Found</h1>
     <h2>お探しのページは見つかりませんでした。</h2>
-    <h3>あなたがアクセスしようとしたページは存在しない可能性が高いです。</h3>
+    <h3>あなたがアクセスしようとしたページは存在しません。</h3>
     <img src="../assets/logo.png" />
   </div>
 </template>
